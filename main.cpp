949f4f4bcc89acd35e8a6f84a19b32f3d57059e9
Gman513 <gregory.havenga@gmail.com>
2016-10-20 18:11:01 +0200

    Additional functionality added. Logging finally functional

--- main.cpp
+++ main.cpp
@@ -1,16 +1,49 @@
 #include "taskmaster.h"
-using namespace std;
 
-void shell();
-
-int main    ()
+int        main(int argc, char *argv[])
 {
+    if (argc < 2)
+    {
+        cout << "Taskmaster requires you to input a .yaml config file" << endl;
+        return (0);
+    }
+    if (argv[2] != NULL)
+        init(argv[2]);
+    else
+        init(NULL);
     shell();
 }
 
-void shell ()
+void       init(char *filename)
 {
-    char*  input;
+    logfile = new ofstream;
+    if (filename != NULL)
+        logfile->open(filename, ios::out | ios::app);
+    else
+        logfile->open("logfile.txt", ios::out | ios::app);
+    cout << "Logfile Succesfully Opened\n";
+    *logfile << "Logfile Succesfully Opened\n";
+    logfile->flush();
+
+}
+
+const std::string currentDateTime() 
+{
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+
+    tstruct = *localtime(&now);
+    // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
+    // for more information about date/time format
+    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+
+    return buf;
+}
+
+void       shell()
+{
+    char   *input;
 
     input = (char *)calloc(256, sizeof(char));
     cout << "TM > ";
@@ -21,6 +54,8 @@ void shell ()
         {
             cout << "TM > Exiting Taskmaster. Have a nice day." << endl;
             free(input);
+            *logfile << currentDateTime() << " User exited Taskmaster\n";
+            logfile->close();
             exit(0);
         }
         else
--- taskmaster.h
+++ taskmaster.h
@@ -2,7 +2,19 @@
 # define __TASKMASTER__
 
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <cstring>
+#include <ctime>
+#include <cstddef>
+#include <time.h>
+
+/*global variables*/
+std::ofstream *logfile;
+
+using namespace std;
+
+void init(char *filename);
+void shell();
 
 #endif
