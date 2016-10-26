bf4bd3bd846718917be8123c727eb8043ca60373
OddballGreg <ghavenga@e5r7p4.jnb.42.fr>
2016-10-26 09:35:46 +0200

    Corrected merge mistakes

--- TestProcess.class.cpp
+++ TestProcess.class.cpp
@@ -3,8 +3,6 @@
 
 class Process {
     char    *lcmd;          //launch command. How the program should be launched. E.g. ./desktop/folder/program arg1 arg2
-    char    *logfile;
-    char    *lcmd;          //launch command. How the program should be launched. E.g. ./desktop/folder/program arg1 arg2
     long    pcount;         //process count
     bool    autostart;
     bool    rstart_cond;    //contingent on use during alg.
@@ -13,8 +11,8 @@ class Process {
     long    retry;
     int     exitsig;
     int     killwait;
-    bool    pid_logging;  ??      //option dicating if the output from the process should be saved or not.
-    char    *pid_logfile;  ??      //if pid_logging is set to true, what file should the output be saved to.
+    bool    pid_logging;     //option dicating if the output from the process should be saved or not.
+    char    *pid_logfile;    //if pid_logging is set to true, what file should the output be saved to.
     char    *env_vars;
     char    *wrk_dir;
     mode_t  umask;
