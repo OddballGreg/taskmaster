4c363c0b94d25d677b8347c597b37e43a9ef6054
Stuart ALLEN <sallen@e5r8p1.jnb.42.fr>
2016-10-31 11:37:11 +0200

    fully updated attribs. parsing 100%

--- phpversion/class data/main.php
+++ phpversion/class data/main.php
@@ -1,44 +1,91 @@
 <?php
 
 class Process {
-    private $_pid = false;
-    private $_lcmd = false;          //launch command. How the program should be launched. E.g. ./desktop/folder/program arg1 arg2
-    private $_pcount = false;         //process count
-    private $_autostart = false;
-    private $_rstart_cond = false;    //contingent on use during alg.
-    private $_exitcodes = false;
-    private $_startwait = false;
-    private $_retry = false;
-    private $_exitsig = false;
-    private $_killwait = false;
-    private $_pid_logging = false;     //option dicating if the output from the process should be saved or not.
-    private $_pid_logfile = false;    //if pid_logging is set to true, what file should the output be saved to.
-    private $_env_vars = false;
-    private $_wrk_dir = false;
-    private $_umask = false;
-    private $_status = false;
-    private $_restartMe = false;
-    private $_child = false;
-    private $_name = false;
+    private $_name;
+    private $_pid;
+    private $_lcmd;
+    private $_pcount;
+    private $_autostart;
+    private $_rstart_cond;
+    private $_exitcodes;
+    private $_startwait;
+    private $_retry;
+    private $_exitsig;
+    private $_killwait;
+    private $_pid_logging;
+    private $_pid_logfile;
+    private $_env_vars;
+    private $_wrk_dir;
+    private $_umask;
+    private $_status;
+    private $_restartMe;
+    private $_child;
+    private $_attribStat;
 
     public function __construct($kwargs) {
         //echo "constructor".PHP_EOL;
-        $this->name = $kwargs[0][0];
-        $this->_attrib_00 = $kwargs[1][1];  //1, not 0, to grep actual value and assign it
-        $this->_attrib_01 = $kwargs[2][1];
+        $this->_name = $kwargs[0][0];
+        $this->_pid = trim($kwargs[1][1],";");
+        $this->_lcmd = trim($kwargs[2][1],";");
+        $this->_pcount = trim($kwargs[3][1],";");
+        $this->_autostart = trim($kwargs[4][1],";");
+        $this->_rstart_cond = trim($kwargs[5][1],";");
+        $this->_exitcodes = trim($kwargs[6][1],";");
+        $this->_startwait = trim($kwargs[7][1],";");
+        $this->_retry = trim($kwargs[8][1],";");
+        $this->_exitsig = trim($kwargs[9][1],";");
+        $this->_killwait = trim($kwargs[10][1],";");
+        $this->_pid_logging = trim($kwargs[11][1],";");
+        $this->_pid_logfile = trim($kwargs[12][1],";");
+        $this->_env_vars = trim($kwargs[13][1],";");
+        $this->_wrk_dir = trim($kwargs[14][1],";");
+        $this->_umask = trim($kwargs[15][1],";");
+        $this->_status = trim($kwargs[16][1],";");
+        $this->_restartMe = trim($kwargs[17][1],";");
+        $this->_child = trim($kwargs[18][1],";");
     }
     
     public function __destruct() {
         //echo "destructor".PHP_EOL;
     }
 
+    public function adjust($pargs) {
+        foreach ($pargs as $arg => $value) {
+            foreach ($this->_attribStat as $process) {
+                if (strncmp($process,$arg,strlen($process)) == 0) {
+                    $process = $value;
+                }
+            }
+        }
+    }
+
     public function getName() {
-        return $this->name;
+        return $this->_name;
     }
 
     public function attribStat() {
-        $attribStat = array($this->_attrib_00,$this->_attrib_01);
-        return $attribStat;
+        $this->_attribStat = array(
+                    $this->_name,
+                    $this->_pid,
+                    $this->_lcmd,
+                    $this->_pcount,
+                    $this->_autostart,
+                    $this->_rstart_cond,
+                    $this->_exitcodes,
+                    $this->_startwait,
+                    $this->_retry,
+                    $this->_exitsig,
+                    $this->_killwait,
+                    $this->_pid_logging,
+                    $this->_pid_logfile,
+                    $this->_env_vars,
+                    $this->_wrk_dir,
+                    $this->_umask,
+                    $this->_status,
+                    $this->_restartMe,
+                    $this->_child,
+            );
+        return $_attribStat;
     }
 
     public function start($command) {
@@ -81,6 +128,7 @@ function initShell($processList) {
             foreach ($processList as $process) {
                 echo $process->getName().PHP_EOL;
                 print_r($process->attribStat());
+                var_dump($process);
             }
         }
         echo "<taskmaster/> ";
@@ -103,5 +151,4 @@ $processList = initData($handle);
 fclose($handle);
 welcome($processList);
 initShell($processList);
-
 ?>
