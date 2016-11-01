8edd02a20bd42d5a646fdc9c7f3c9eb167c4018e
OddballGreg <ghavenga@e5r8p3.jnb.42.fr>
2016-11-01 11:29:03 +0200

    process handling scripts refactored

--- phpversion/phandling.php
+++ phpversion/phandling.php
@@ -2,43 +2,43 @@
 
 function		maintain()
 {
-/*
 	$index = -1;
-	while ($GLOBALS['processes'][++index] != NULL)
+	while ($GLOBALS['processes'][++$index] != NULL)
 	{
+		$process = $GLOBALS['processes'][$index];
 		//request status
 		//if offline, request restart boolean setting
 		//attempt to restart if true
-		if (processes[index]->status() == FALSE)
+		if ($process->status() == FALSE)
 		{
-			*logFile << currentDateTime() << " " << processes[index]->name<< " " << processes[index]->pid << " Reported as OFFLINE\n";
-			if (processes[index]->rstart_cond == TRUE)
+			log_message($process->_attribStat['name'] . " " . $process->_attribStat['pid'] . " Reported as OFFLINE\n");
+			if ($process->_attribStat['rstart_cond'] == TRUE)
 			{
-				*logFile << currentDateTime() << " " << processes[index]->name<< " " << processes[index]->pid << " Attempting To Restart Process\n";
-				processes[index]->start();
+				log_message($process->_attribStat['name'] . " " . $process->_attribStat['pid'] . " Attempting To Restart Process\n");
+				$process->start();
 			}
 		}
 		//request restartMe info
 		//if true, restart the program(s)
-		if (processes[index]->restartMe == TRUE)
+		if ($process->_attribStat['restartMe'] == TRUE)
 		{
-			*logFile << currentDateTime() << " " << processes[index]->name<< " " << processes[index]->pid << " Process detected as flagged for Restarting\n";
-			*logFile << currentDateTime() << " " << processes[index]->name<< " " << processes[index]->pid << " Attempting To Restart Process\n";
-			processes[index]->restartMe == FALSE);
-			processes[index]->restart();
+			log_message($process->_attribStat['name'] . " " . $process->_attribStat['pid'] . " Process detected as flagged for Restarting\n");
+			log_message($process->_attribStat['name'] . " " . $process->_attribStat['pid'] . " Attempting To Restart Process\n");
+			$process->_attribStat['restartMe'] == FALSE;
+			$process->restart();
 		}
-	}	*/
+	}	
 }
 
 function		autostart()
 {
-	/*int		index;
-
-	index = -1;
-	while (processes[++index] != NULL)
-		if (processes[index]->autostart == TRUE)
-			processes[index]->start();
-			*/
+	$index = -1;
+	while ($GLOBALS['processes'][++$index] != NULL)
+	{
+		$process = $GLOBALS['processes'][$index];
+		if ($process->_attribStat['autostart'] == TRUE)
+			$process->start();
+	}
 }
 
 ?>
